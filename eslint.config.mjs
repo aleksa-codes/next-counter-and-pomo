2baf284efba302c4817f016967f1f117a9ecc548
aleksa-codes <hello@aleksa.codes>
2025-12-12 18:12:29 +0100

    Migrate to Tailwind v4

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import nextTypescript from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+
+const eslintConfig = defineConfig([
+  ...nextTypescript,
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  }
+]);
+
+export default eslintConfig;
